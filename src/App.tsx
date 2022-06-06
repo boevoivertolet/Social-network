5ca0c4ca7523c295404e256aed8196a7f1b99286
boevoivertolet <bjha78rus@yandex.ru>
2022-06-06 15:20:47 +0300

    12

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
+import {Header} from './components/Header';
 
 function App() {
     return (
-        <div className='app-wrapper'>
-            <header >
-                <img
-                    src='https://phototass1.cdnvideo.ru/width/1200_4ce85301/tass/m2/uploads/i/20220219/6621365.jpg'
-                    alt=""/>
-                <span><em>Benicio Monserrate Rafael del Toro Sánchez</em></span>
-            </header>
+        <div className="app-wrapper">
+          <Header/>
             <nav>
                 <div><a href="#">Profile</a></div>
                 <div><a href="#">Messages</a></div>
@@ -18,10 +14,20 @@ function App() {
                 <div><a href="#">Settings</a></div>
 
             </nav>
-            <div className='content'>
-                main content
-                <img src="https://images.wallpaperscraft.ru/image/single/gory_more_okean_oblaka_noch_96938_3840x2400.jpg" alt=""/>
+            <div className="content">
+                <img
+                    src="https://images.wallpaperscraft.ru/image/single/gory_more_okean_oblaka_noch_96938_3840x2400.jpg"
+                    alt=""/>
+                <div className="ava-description">
+                    ava + description
                 </div>
+                <div className="myPosts">
+                    My posts
+                    <div className="newPost">new post</div>
+                    <div className="post"> post 1</div>
+                    <div className="post"> post 2</div>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function Header() {
+    return (
+        <header>
+            <img
+                src="https://phototass1.cdnvideo.ru/width/1200_4ce85301/tass/m2/uploads/i/20220219/6621365.jpg"
+                alt=""/>
+            <span><em>Benicio Monserrate Rafael del Toro Sánchez</em></span>
+        </header>
+    )
+}
