47ca16c06171fe04253dd7a88888cf3a6f48a30e
boevoivertolet <bjha78rus@yandex.ru>
2022-06-06 14:57:13 +0300

    Benicio Monserrate Rafael del Toro Sánchez

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
-
 import './App.css';
 
 function App() {
     return (
         <div className='app-wrapper'>
-            <header>
+            <header >
                 <img
-                    src="https://coub-attachments.akamaized.net/coub_storage/coub/simple/cw_timeline_pic/35f879230a6/fc6ae2bde8cb36b8ab296/1492800456_image.jpg"
-                    alt="x"/>
+                    src='https://phototass1.cdnvideo.ru/width/1200_4ce85301/tass/m2/uploads/i/20220219/6621365.jpg'
+                    alt=""/>
+                <span><em>Benicio Monserrate Rafael del Toro Sánchez</em></span>
             </header>
             <nav>
-                <div>Profile</div>
-                <div>Messages</div>
+                <div><a href="#">Profile</a></div>
+                <div><a href="#">Messages</a></div>
+                <div><a href="#">News</a></div>
+                <div><a href="#">Music</a></div>
+                <div><a href="#">Settings</a></div>
+
             </nav>
-            <div>main content</div>
+            <div className='content'>
+                main content
+                <img src="https://images.wallpaperscraft.ru/image/single/gory_more_okean_oblaka_noch_96938_3840x2400.jpg" alt=""/>
+                </div>
         </div>
     );
 }
