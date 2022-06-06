6689212cb5db4f9b4f1f3520e8eab62db631df46
boevoivertolet <bjha78rus@yandex.ru>
2022-06-06 17:26:46 +0300

    15

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import  './Navbar.module.css'
+
+
+export function Navbar() {
+    return (
+        <nav>
+            <div><a href="src/components/Navbar/Navbar#">Profile</a></div>
+            <br/>
+            <div><a href="src/components/Navbar/Navbar#">Messages</a></div>
+            <br/>
+            <div><a href="src/components/Navbar/Navbar#">News</a></div>
+            <br/>
+            <div><a href="src/components/Navbar/Navbar#">Music</a></div>
+            <br/>
+            <div ><a href="src/components/Navbar/Navbar#">Settings</a></div>
+        </nav>
+    )
+
+}
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './Profile.module.css'
 
 
 export function Profile() {
     return (
-        <div className="content">
+        <div className={styles.content}>
             <img
                 src="https://images.wallpaperscraft.ru/image/single/gory_more_okean_oblaka_noch_96938_3840x2400.jpg"
                 alt=""/>
