8b24077435c8268f1b3c18b25ae778f0fafbce05
boevoivertolet <bjha78rus@yandex.ru>
2022-06-07 16:53:09 +0300

    22

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import styles from './Dialogs.module.css'
 
 export function Dialogs() {
     return (
-        <div>
-            <span>Dialogs</span>
+
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                <div className={styles.dialog}>
+                    <span>Ulya</span>
+                </div>
+                <div className={styles.dialog}>
+                    <span>Taras</span>
+                </div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur laboriosam nam nulla quibusdam unde. A esse quia quo sit tenetur! Accusamus architecto aspernatur assumenda at consequatur  vero voluptate voluptates?
+                </div>
+                <div className={styles.message}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, non?
+                </div>
+
+            </div>
         </div>
+
+
     )
 }
